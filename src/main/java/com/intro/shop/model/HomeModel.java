f715c1eb2208731fc6e79dcdae1768541c0de7e0
User <vagifhuseynov95@gmail.com>
2026-07-29 06:35:04 +0400

    first test

---
+++ src/main/java/com/intro/shop/model/HomeModel.java
@@ -0,0 +1,38 @@
+package com.intro.shop.model;
+
+import org.hibernate.annotations.ConverterRegistrations;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class HomeModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String firstName;
+
+    @Column(nullable = false, length = 100)
+    private String lastName;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(length = 30)
+    private String phone;
+
+    @Column(nullable = false)
+    private final boolean active = true;
+}
